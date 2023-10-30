25738de9e52781ba0c71f71a502a1a93837da19e
isfandyarIOT <isfandyar.qureshi@dataminfo.io>
2023-10-30 15:44:56 +0500

    mcp3002 library used last working main

--- esp32_mcp32008/src/main.c
+++ esp32_mcp32008/src/main.c
@@ -12,11 +12,11 @@
 
 // #define TAG "MCP3208"
 #define NUMBER_OF_SAMPLES 1048
-
+double ICAL = 90.9;
 static const char *TAG = "current_sensor";
 static const char *VOLTTAG = " Voltage";
 
-double offsetI;
+double offsetI = 2048;
 double filteredI;
 double sqV, sumV, sqI, sumI, instP, sumP;
 double realPower, apparentPower, powerFactor, Vrms;
@@ -26,12 +26,12 @@ static spi_device_handle_t spi2;
 MCP_t dev;
 
 double calcIrms_with_mcp3208(int numberOfSamples) {
-  uint16_t sampleI = 0;
+  int sampleI = 0;
   double sumI = 0;
 
   for (int n = 0; n <= numberOfSamples; n++) {
-    sampleI = mcpReadData(&dev, 0);
-
+    sampleI = mcpReadData(&dev, 1);
+    // ESP_LOGI(TAG, "Single_ended ch0 value=%d", sampleI);
     offsetI = (offsetI + (sampleI - offsetI) / 1024);
     filteredI = sampleI - offsetI;
 
@@ -39,7 +39,7 @@ double calcIrms_with_mcp3208(int numberOfSamples) {
     sumI += sqI;
   }
 
-  double I_RATIO = 90.9 * ((3300 / 1000.0) / (4096));
+  double I_RATIO = ICAL * ((3300 / 1000.0) / (4096));
   double Irms = I_RATIO * sqrt(sumI / numberOfSamples);
 
   sumI = 0;
@@ -48,13 +48,14 @@ double calcIrms_with_mcp3208(int numberOfSamples) {
 }
 
 void app_main() {
-
+  int16_t value;
   // Initialize the SPI bus
   mcpInit(&dev, model, GPIO_NUM_19, GPIO_NUM_23, GPIO_NUM_18, GPIO_NUM_5,
           MCP_SINGLE);
 
   while (1) {
-    double Irms_main = calcIrms_with_mcp3208(20000);
+    double Irms_main = calcIrms_with_mcp3208(1048);
+    // value = mcpReadData(&dev, 1);
     ESP_LOGI("mcp320x", "Current: %f mA", Irms_main);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
